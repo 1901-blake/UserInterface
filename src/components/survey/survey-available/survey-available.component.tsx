408df4cab7efc2569d8d4e48b2e9571cce3021ff
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-03-08 10:52:17 -0500

    removed survey internal and added assign component

--- src/components/survey/survey-available/survey-available.component.tsx
+++ src/components/survey/survey-available/survey-available.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SurveyInternalComponent from '../survey-internal/survey-internal.component';
+import { Table } from 'reactstrap';
 
 export class SurveyAvailableComponent extends React.Component {
     constructor(props) {
@@ -8,10 +8,22 @@ export class SurveyAvailableComponent extends React.Component {
 
     render() {
         return (
-            <SurveyInternalComponent />
+            <Table>
+            <thead className="rev-background-color">
+              <tr>
+                <th>Surveys To Take</th>
+                <th>Start Survey</th>
+              </tr>
+            </thead>
+            <tbody>
+                  <tr className="rev-table-row">
+                    <td>amigo</td>
+                    <td>sup</td>
+                  </tr>
+            </tbody>
+          </Table>
         );
     }
 }
-
 
 export default SurveyAvailableComponent;
